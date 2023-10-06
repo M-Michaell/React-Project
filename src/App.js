ed97d3aa46f4cf08c1e2efd1e5ec907af52cdd3f
ShazaShaban <shathashabanalberry@gmail.com>
2023-10-06 05:49:28 +0300

    Search-Navbar-Header Components 1st edition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Search from "./components/Search/Search";
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router/router";
+import { LanguageContext } from "./Context/language";
 function App() {
   return (
-    <>
-      <div>
-        <h1>hello</h1>
+      <div
+     >
+        {/* <BrowserRouter>
+          <Router/>
+        </BrowserRouter> */}
+        <Header/>
       </div>
-    </>
+
   );
 }
 
---
+++ src/Router/router.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <Router>
+      <Route path="" element=""/>
+      <Route path="" element=""/>
+      <Route path="" element=""/>
+      <Route path="" element=""/>
+      <Route path="" element=""/>
+      
+    </Router>
+  );
+}
